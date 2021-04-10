d96b12293901b192676198f68680222d1f3577ea
ifoelarbeis <anesya.arbeis@gmail.com>
2021-04-10 15:55:52 +0700

    riwayat pekerjaan

--- fits_latihan_botcam/models/employee.py
+++ fits_latihan_botcam/models/employee.py
@@ -6,12 +6,29 @@ class HrDivisi(models.Model):
 	_name = 'hr.divisi'
 	_description = 'Divisi'
 	
-
 	name = fields.Char(string="Nama Divisi")
 	desc = fields.Char(string="Keterangan")
-
+	
 class HrEmployee(models.Model):
 	_inherit = 'hr.employee'
 	
-	devisi_id = fields.Many2one('hr.divisi', string="Divisi")
+	devisi_id 		= fields.Many2one('hr.divisi', string="Divisi")
+	nik       		= fields.Char(string="NIK")
+	tgl_bergabung 	= fields.Date(string="Tgl Bergabung")
+	agama 			= fields.Selection([('islam','Islam'),('kristen','Kristen'),('katolik','Katolik'),('hindu','Hindu'),
+										('budha','Budha')], string="Agama")
+	riwayat_ids		= fields.One2many('riwayat.pekerjaan','employee_id', string="Riwayat")
+	
+	
+
+class RiwayatPekerjaan(models.Model):
+	_name = 'riwayat.pekerjaan'
+	_description = 'Riwayat Pekerjaan'
+	
+	mulai_bekerja 	= fields.Char(string="Tahun Mulai Bekerja")
+	selesai_bekerja = fields.Char(string="Selesai Bekerja")	
+	nama_perusahaan = fields.Char(string="Nama Perusahaan")
+	res_partner_id 	= fields.Many2one('res.partner',string="Company")
+	jabatan			= fields.Char(string="Jabatan")
+	employee_id		= fields.Many2one('hr.employee', string="Employee")
 	
